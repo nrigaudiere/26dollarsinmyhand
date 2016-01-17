54483cadeec8955c6780984b3a8120c411b00cd9
Nicolas Rigaudiere <nicolas.rigaudiere@gmail.com>
2016-01-17 11:45:32 +0100

    Edited json + added switch lang

--- assets/js/controllers/26.controller.js
+++ assets/js/controllers/26.controller.js
@@ -1,16 +1,28 @@
 twentySixApp.controller('twentySixController', ['$scope', '$http', function ($scope, $http) {
 
-	$scope.lang  = [];
-	$scope.menu  = {};
-	$scope.about = "";
+	$scope.lang 	 = [];
+	$scope.menu  	 = {};
+	$scope.paragraph = "";
 
 
 	$http.get("assets/json/lang_en.json").success(function(data) {
-		$scope.lang.en = data;
-
-		$scope.menu  = data.menu;
-		$scope.about = data.about;
+		$scope.lang.en 	 = data;
+		$scope.menu  	 = data.menu;
+		$scope.paragraph = data.paragraph;
 	});
 
+	$http.get("assets/json/lang_fr.json").success(function(data) {
+		$scope.lang.fr = data;
+	});
+
+	$scope.toFr = function() {
+		$scope.menu 	 = $scope.lang.fr.menu;
+		$scope.paragraph = $scope.lang.fr.paragraph;
+	}
+
+	$scope.toEn = function() {
+		$scope.menu 	 = $scope.lang.en.menu;
+		$scope.paragraph = $scope.lang.en.paragraph;
+	}
 
 }]);
