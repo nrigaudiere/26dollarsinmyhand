3c7c8eb81451c5b80007a92f04dcc15f892563f7
Nicolas Rigaudiere <nicolas.rigaudiere@gmail.com>
2016-01-17 16:47:46 +0100

    Finished angularjs

--- assets/js/controllers/26.controller.js
+++ assets/js/controllers/26.controller.js
@@ -4,6 +4,9 @@ twentySixApp.controller('twentySixController', ['$scope', '$http', function ($sc
 	$scope.menu  	 = {};
 	$scope.paragraph = "";
 
+	$scope.enBold	 = "en boldLang";
+	$scope.frBold	 = "fr";
+
 
 	$http.get("assets/json/lang_en.json").success(function(data) {
 		$scope.lang.en 	 = data;
@@ -18,11 +21,15 @@ twentySixApp.controller('twentySixController', ['$scope', '$http', function ($sc
 	$scope.toFr = function() {
 		$scope.menu 	 = $scope.lang.fr.menu;
 		$scope.paragraph = $scope.lang.fr.paragraph;
+		$scope.enBold	 = "";
+		$scope.frBold	 = "boldLang";
 	}
 
 	$scope.toEn = function() {
 		$scope.menu 	 = $scope.lang.en.menu;
 		$scope.paragraph = $scope.lang.en.paragraph;
+		$scope.enBold	 = "boldLang";
+		$scope.frBold	 = "";
 	}
 
 }]);
